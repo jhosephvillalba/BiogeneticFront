4070d0842b23a148414f0dddfa78fde6af79ac00
jhosephvillalba <enriquevillalba97@outlook.com>
2025-06-11 13:58:17 -0500

    apuntar al backend

--- src/Api/axios.js
+++ src/Api/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://127.0.0.1:8000/api';
+const API_URL = 'http://44.197.38.121:8000/api';
 
 const axiosInstance = axios.create({
   baseURL: API_URL,
